73de5da70bb4d794214544c05410e31250e61c61
jafarjuneidi <jafar@pop-os.localdomain>
2022-12-26 22:02:19 +0300

    zsh & zsh-antidote & starship

---
+++ dot_config/zsh/dot_zsh_plugins.zsh
@@ -0,0 +1,4 @@
+fpath+=( /home/jafar/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions )
+source /home/jafar/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-autosuggestions/zsh-autosuggestions.plugin.zsh
+fpath+=( /home/jafar/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting )
+source /home/jafar/.cache/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-zsh-users-SLASH-zsh-syntax-highlighting/zsh-syntax-highlighting.plugin.zsh
